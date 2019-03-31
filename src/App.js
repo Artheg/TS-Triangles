9f59ce9eecf8b7a2dd97fedfb8e9d48094be828b
Artheg <artheg@gmail.com>
2019-03-31 19:40:19 +0300

    AppRouter added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './styles/App.css';
-import TriangleTypeCheckScreen from './screens/TriangleTypeCheckScreen';
+import AppRouter from './routes/AppRouter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TriangleTypeCheckScreen />
+      <AppRouter />
       </div>
     );
   }
---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import TriangleTypeCheckScreen from "../screens/TriangleTypeCheckScreen";
+
+function AppRouter() {
+  return (
+    <Router>
+        <Route path="/" exact component={TriangleTypeCheckScreen} />
+    </Router>
+  );
+}
+
+export default AppRouter;
